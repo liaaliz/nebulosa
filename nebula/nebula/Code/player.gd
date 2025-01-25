8ce2610746edef56e48ca57519ef391e8da601e1
liaaliz <lializ@proton.me>
2025-01-25 03:41:07 -0300

    feat add metaball shader
    Twice for good measure (for some reason, i forgot to do add the files to
    the commit. it's 3:40 AM, god help me.

--- nebula/nebula/Code/player.gd
+++ nebula/nebula/Code/player.gd
@@ -1,19 +1,13 @@
 extends CharacterBody2D
-const BOOST_VAL := 2000
+const BOOST_VAL := 60
 var orientation
-@onready var reload_timer = Timer.new()
 
-func _ready() -> void:
-	reload_timer.autostart = false
-	reload_timer.wait_time = 1.0
-
-func _unhandled_key_input(event: InputEvent) -> void:
-	boost()
-		
 func _physics_process(delta: float) -> void:
-	orientation = Vector2.RIGHT.rotated(rotation)
 	var mouse_pos = get_global_mouse_position()
 	look_at(Vector2(mouse_pos.x, mouse_pos.y))
+	orientation = Vector2.RIGHT.rotated(rotation)
+	
+	boost()
 	move_and_slide()
 	
 	#edge case:
@@ -23,9 +17,4 @@ func _physics_process(delta: float) -> void:
 
 func boost():
 	if Input.is_action_pressed("boost"):
-		velocity += orientation * BOOST_VAL
-		velocity = velocity.limit_length(200.0)
-		
-func shoot():
-	pass
-	#if Input.is_action_just_pressed("shoot"):
+		velocity = lerp(velocity, orientation * BOOST_VAL, 0.3)
